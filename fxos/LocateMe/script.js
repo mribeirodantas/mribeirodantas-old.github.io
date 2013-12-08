a5c93d7a1071c5c54ef2358963d347a570cda10e
Marcel Ribeiro Dantas <mribeirodantas@fedoraproject.org>
2013-12-08 16:25:05 -0300

    fxos

--- fxos/LocateMe/script.js
+++ fxos/LocateMe/script.js
@@ -1,13 +1,17 @@
 window.onload = function() {
-    if (navigator.mozApps) {
-        var request = navigator.mozApps.getSelf();
+  // Offer to install ourselves
+  if (navigator.mozApps) {
+    var request = navigator.mozApps.getSelf();
+    request.onsuccess = function() {
+      if (!this.result) {
+        request = navigator.mozApps.install(location.protocol + "//" + location.host + location.pathname + "manifest.webapp");
         request.onsuccess = function() {
-            if (!this.result) {
-                request = navigator.mozApps.install(location.protocol + "//" + location.host + location.pathname + "manifest.webapp");
-                request.onerror = function() {
-                    console.log("Install failed: " + this.error.name);
-                };
-            }
+          this.result.launch();
         };
-    }
+        request.onerror = function() {
+          console.log("Install failed: " + this.error.name);
+        };
+      }
+    };
+  }
 };
